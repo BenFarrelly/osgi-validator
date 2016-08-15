7234a8559e467ada566381e102d7826c2f39d11d
Benjamin Farrelly <bfar697@aucklanduni.ac.nz>
2016-08-16 11:28:48 +1200

    Updates and starts bundles
    -Updates bundle in framework
    -If unable to resolve then feedback given
    -If able to resolve bundle then starts
    TODO:
    -Give more expressive feedback
    -Remove testing print statements
    -Add hooking possibility

--- com.validator.shell/src/main/java/com/validator/shell/ShellCommands.java
+++ com.validator.shell/src/main/java/com/validator/shell/ShellCommands.java
@@ -28,86 +28,105 @@ public class ShellCommands {
 	//This class is a bundle which gives a customised version of the Gogo shell giving the extra commands in validation.
 	private final BundleContext bundleContext;
 	private ConfigurationAdmin configAdmin;
-	
+
 	CommandProcessorImpl cp;
 	public ShellCommands(BundleContext bundleContext){
-		
+
 		this.bundleContext = bundleContext;
-		
+
 		//getConfigAdmin();
 	}
+	//TODO bring in update code
 	@Descriptor("Validate a bundle for updating.")
 	public void update(
 			@Descriptor("The bundle number that is the 'old' bundle")int bundleNumber, 
 			@Descriptor("The path to the new bundle")String path){
 		//This method is used to validate that a bundle can indeed update safely - in that nomethod and typemismatch exceptions will not occur. 
 		//These are typically unchecked by osgi.
-		
+
 		//Firstly the classes are taken from the Bundle
 		try{
-			
-		JarToClasses jar = new JarToClasses(path);
-		
-		ArrayList<Class<?>> classes = jar.classes;
-		String bundlePath = InterconnectionChecker.getBundlePathFromNumber(bundleNumber); //TODO check if this is actually bening used
-		//TODO take the code from the tests
-		JarToClasses jar2 = new JarToClasses(bundlePath);
-		ArrayList<Class<?>> classes2 = jar2.classes;
-		HashMap<Class<?>, HashMap<Method, ComparisonStatus>> methodEqualityMap = MapAnalyser.updateJarAnalysis(classes, classes2);
-		//Now to give a response regarding the results
-		Set<Class<?>> classSet = methodEqualityMap.keySet();
-		Iterator<Class<?>> classIter = classSet.iterator();
-		Class<?> tempClass = null;
-		HashMap<Method, ComparisonStatus> tempMap;
-		int classSize = classSet.size();
-		int tempClassSize = 0;
-		System.out.println("Making it to the start of the iterating loop, should make it through" + classSize + " times");
-		int i = 0;
-		while(classIter.hasNext()){
-			i++;
-			tempClass = classIter.next();
-			System.out.println("Making it through this loop, iteration: " + i);
-			if(tempClass != null){
-				System.out.println("Getting into tempClass block for class: " + tempClass);
-			//assertNotNull("Class isn't null", tempClass);
-			tempMap = methodEqualityMap.get(tempClass);
-			//This is going to get ugly
-			
-				if(tempMap.containsValue(ComparisonStatus.NO_METHOD)){
-					//ComparisonStatus classStatus = tempMap.get(tempClass);
-					System.out.println(tempClass.getName() + " has a missing method, solve this before updating bundle");
-				
-				}else if(tempMap.containsValue(ComparisonStatus.TYPE_MISMATCH)){
-					System.out.println(tempClass.getName() + " has a type mismatch solve this before updating bundle");
-				
-				}else if(tempMap.containsValue(ComparisonStatus.SUB_TYPED)){
-					tempClassSize++;
-					System.out.println(tempClass.getName() + " has a subtyped method, bundle is fine, just letting you know!");
-				}else if(tempMap.containsValue(ComparisonStatus.NOT_EQUAL)){
-					System.out.println(tempClass.getName() + " has a non-equal method solve this before updating bundle");
-				} else if (tempMap.containsValue(ComparisonStatus.EQUAL)){
+
+			JarToClasses jar = new JarToClasses(path);
+
+			ArrayList<Class<?>> classes = jar.classes;
+			String bundlePath = InterconnectionChecker.getBundlePathFromNumber(bundleNumber); 
+
+			JarToClasses jar2 = new JarToClasses(bundlePath);
+			ArrayList<Class<?>> classes2 = jar2.classes;
+			HashMap<Class<?>, HashMap<Method, ComparisonStatus>> methodEqualityMap = MapAnalyser.updateJarAnalysis(classes, classes2);
+			//Now to give a response regarding the results
+			Set<Class<?>> classSet = methodEqualityMap.keySet();
+			Iterator<Class<?>> classIter = classSet.iterator();
+			Class<?> tempClass = null;
+			HashMap<Method, ComparisonStatus> tempMap;
+			int classSize = classSet.size();
+			int tempClassSize = 0;
+			System.out.println("Making it to the start of the iterating loop, should make it through" + classSize + " times");
+			int i = 0;
+			Bundle updatingBundle = bundleContext.getBundle("file:" + path);
+
+			while(classIter.hasNext()){
+				i++;
+				tempClass = classIter.next();
+				System.out.println("Making it through this loop, iteration: " + i);
+				if(tempClass != null){
+					System.out.println("Getting into tempClass block for class: " + tempClass);
+					//assertNotNull("Class isn't null", tempClass);
+					tempMap = methodEqualityMap.get(tempClass);
+					//This is going to get ugly
+
+					if(tempMap.containsValue(ComparisonStatus.NO_METHOD)){
+						//ComparisonStatus classStatus = tempMap.get(tempClass);
+						System.out.println(tempClass.getName() + " has a missing method, solve this before updating bundle");
+
+					}else if(tempMap.containsValue(ComparisonStatus.TYPE_MISMATCH)){
+						System.out.println(tempClass.getName() + " has a type mismatch solve this before updating bundle");
+
+					}else if(tempMap.containsValue(ComparisonStatus.SUB_TYPED)){
+						tempClassSize++;
+						System.out.println(tempClass.getName() + " has a subtyped method, bundle is fine, just letting you know!");
+
+					}else if(tempMap.containsValue(ComparisonStatus.NOT_EQUAL)){
+						System.out.println(tempClass.getName() + " has a non-equal method solve this before updating bundle");
+					} else if (tempMap.containsValue(ComparisonStatus.EQUAL)){
 						tempClassSize++;
 						System.out.println("ComparisonStatus = EQUAL");
-				} else {
-					System.out.println("Did not have a comparison status for some reason");
+					} else {
+						System.out.println("Did not have a comparison status for some reason");
+					}
+
 				}
-			
 			}
-		}
-		if(tempClassSize == classSize){
-			System.out.println("The bundle has been validated and can be updated!");
-		} else {
-			System.out.println("Revise this bundle before updating");
-		}
-		}catch(Exception e){
-			System.out.println("That path returned a FileNotFoundException, please revise the path you provided");
+			if(tempClassSize == classSize){
+				System.out.println("The bundle has been validated and can be updated!");
+				updatingBundle.update();
+				System.out.println("Updated bundle "+ bundleNumber + " to " + path + " successfully!");
+
+				updatingBundle.start();
+				System.out.println("Bundle " + bundleNumber+ " has started successfully");
+			} else {
+				System.out.println("Revise this bundle before updating");
+			}
+		}catch(BundleException e){
+			if(e.getType() == BundleException.RESOLVE_ERROR){
+				System.out.println("Resolve error, ensure you have access to all of the relevant packages the bundle is importing");
+				System.out.println("Bundle is installed, solve resolver issue before starting");
+			} else {
+				System.out.println("A Bundle exception because : " + e.getType());
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			System.out.println("Some random exception :(");
+			e.printStackTrace();
 		}
 	}
 	public void update(
 			@Descriptor("The number of the bundle which you wish to validate")int bundleNumber){//Maybe not relevant...
-		
-		
+
+
 	}
+	//TODO bring in update code
 	@Descriptor("Used for checking that a bundle implements a service correctly, give either paths or bundle numbers for the validating bundle and the bundle that contains the service.")
 	public void interconnection(
 			@Descriptor("The path to the bundle being updated")String path, 
@@ -116,6 +135,7 @@ public class ShellCommands {
 		JarToClasses serviceBundle = new JarToClasses(path2);
 		ArrayList<Class<?>> serviceClasses = serviceBundle.classes;
 		Class<?> service = null;
+		Bundle updatingBundle = bundleContext.getBundle("file:"+ path);
 		for(Class<?> clazz: serviceClasses){//This only checks one bundle, TODO consider adding implementation for more than one service
 			if(clazz.isInterface()){
 				service = clazz;
@@ -127,17 +147,37 @@ public class ShellCommands {
 			serviceIsCorrect = InterconnectionChecker.isServiceUsedCorrectly(service, path); // need to make new implementation that takes a path
 		}
 		if(serviceIsCorrect == ComparisonStatus.EQUAL || serviceIsCorrect == ComparisonStatus.SUB_TYPED){
-			System.out.println("Passed validation against this service, feel free to update the bundle safely.");
+			//System.out.println("Passed validation against this service, feel free to update the bundle safely.");
+			try {
+				updatingBundle.update();
+				System.out.println("Updated " + path + " was successful!");
+				updatingBundle.start();
+				System.out.println("Bundle " + updatingBundle.getBundleId()+ " has started successfully!");
+				} catch (BundleException e) {
+					if(e.getType() == BundleException.RESOLVE_ERROR){
+						System.out.println("Resolve error, ensure you have access to all of the relevant packages the bundle is importing");
+						System.out.println("Bundle is installed, solve resolver issue before starting");
+					} else {
+						System.out.println("A Bundle exception because : " + e.getType());
+						e.printStackTrace();
+				
+					}
+				}
+					catch(Exception e){
+						System.out.println("Some other exception");
+						e.printStackTrace();
+				}
+
 		} else {
 			System.out.print("Service was not correct in its usage, revise your usage of this service before updating");
 		}
 	}
-	
+	//Complete
 	public void interconnection(
 			@Descriptor("Bundle number of the bundle that contains the service")int bundleNumber,
-			@Descriptor("Path to the bundle that is being checked for validation")String bundlePath) throws BundleException{
-		
-		
+			@Descriptor("Path to the bundle that is being checked for validation")String bundlePath){
+
+
 		String path = InterconnectionChecker.getBundlePathFromNumber(bundleNumber);
 		//JarToClasses serviceBundle = new JarToClasses(path);
 		JarToClasses bundle = new JarToClasses(path);
@@ -146,7 +186,7 @@ public class ShellCommands {
 		for(Class<?> clazz: serviceClasses){
 			if(clazz.isInterface()){
 				services.add(clazz);
-				
+
 			}
 		}
 		ComparisonStatus serviceIsCorrect = null;
@@ -156,35 +196,66 @@ public class ShellCommands {
 		System.out.println("ComparisonStatus of this interface is: " + serviceIsCorrect);
 		if(serviceIsCorrect == ComparisonStatus.EQUAL){
 			System.out.println("Passed validation against this service, feel free to update the bundle safely");
-			Bundle[] bundles = bundleContext.getBundles();
-			Bundle updatingBundle = null;
-				for(Bundle b : bundles){
-					if(b.getBundleId() == bundleNumber){
-						updatingBundle = b;
-						break;
+			//Bundle[] bundles = bundleContext.getBundles();
+			Bundle updatingBundle = bundleContext.getBundle("file:"+bundlePath);
+			//				for(Bundle b : bundles){
+			//					if(b.getBundleId() == bundleNumber){
+			//						updatingBundle = b;
+			//						break;
+			//					}
+			//				}
+			if(updatingBundle != null){
+				try {
+					updatingBundle.update();
+
+					System.out.println("Bundle " + bundlePath +  " updated");
+					updatingBundle.start();
+					System.out.println("Bundle " + updatingBundle.getBundleId() + " has started successfully!");
+				} catch (BundleException e) {
+					if(e.getType() == BundleException.RESOLVE_ERROR){
+						System.out.println("Resolve error, ensure you have access to all of the relevant packages the bundle is importing");
+						System.out.println("Bundle is installed, solve resolver issue before starting");
+					} else {
+						System.out.println("A Bundle exception because : " + e.getType());
+						e.printStackTrace();
 					}
 				}
-			if(updatingBundle != null){
-				updatingBundle.update();
-				System.out.println("Bundle" + bundleNumber + "updated");
+
+
 			}
-			
-//			try {
-//				//commandProcessor = getOsgiService(CommandProcessor.class);
-//				ThreadIOImpl t = new ThreadIOImpl();
-//				t.start();
-//				cp = new CommandProcessorImpl(t);
-//				CommandSession csesh = cp.createSession(System.in, System.out, System.err);
-//				csesh.execute("felix:update "+ bundleNumber);
-//			} catch (Exception e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
+
+
+			//			try {
+			//				//commandProcessor = getOsgiService(CommandProcessor.class);
+			//				ThreadIOImpl t = new ThreadIOImpl();
+			//				t.start();
+			//				cp = new CommandProcessorImpl(t);
+			//				CommandSession csesh = cp.createSession(System.in, System.out, System.err);
+			//				csesh.execute("felix:update "+ bundleNumber);
+			//			} catch (Exception e) {
+			//				// TODO Auto-generated catch block
+			//				e.printStackTrace();
+			//			}
 		}else if(serviceIsCorrect == ComparisonStatus.SUB_TYPED){
 			System.out.println("Passed validation, although the service is using a subtype.");
+			Bundle updatingBundle = bundleContext.getBundle("file:"+bundlePath);
+			if(updatingBundle != null){
+				try {
+					updatingBundle.update();
+				} catch (BundleException e) {
+					if(e.getType() == BundleException.RESOLVE_ERROR){
+						System.out.println("Resolve error, ensure you have access to all of the relevant packages the bundle is importing");
+						System.out.println("Bundle is installed, solve resolver issue before starting");
+					} else {
+						System.out.println("A Bundle exception because : " + e.getType());
+						e.printStackTrace();
+					}
+				}
+				System.out.println("Bundle " + bundlePath +  " updated");
+			}
 		} else {
 			System.out.println("Service was not correct in usage, revise your usage of this service before updating.");
 		}
 	}
-	
+
 }
